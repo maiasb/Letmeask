64933af07a8b55eca8dd8430db06a609a935a3bd
Caio Maia <caio.maiasb@gmail.com>
2021-07-05 18:40:07 -0300

    Tela de criação de sala finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // IMPORTANDO COMPONENTE BUTTON
 import { Button } from './components/Button'
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <Home />
+    <NewRoom />
   )
 }
 
