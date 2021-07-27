2b6f0f21035041dee6d8b8a5333be3e69198245a
Caio Maia <caio.maiasb@gmail.com>
2021-07-27 12:54:22 -0300

    Fluxo de entrada em uma sala. Se ela existe ou não, entra na sala, ou retorna mensagem de erro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,15 @@ import { Button } from '../components/Button'
 import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss'
+import { FormEvent } from 'react';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
     // ADICIONANDO HOOK DE ROTA NA CONSTANTE
     const history = useHistory();
-
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     // CRIAÇÃO DE FUNÇÃO PARA REDIRECIONAMENTO DA ROTA
     async function handleCreateRoom() {
@@ -25,6 +28,23 @@ export function Home() {
         // REDIRECIONA PARA A ROTA
         history.push("/rooms/new");
     };
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`/rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`)
+    }
 
     return (
         <div id="page-auth">
@@ -46,10 +66,12 @@ export function Home() {
 
                     <div className="separator">ou entre em uma sala</div>
 
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
